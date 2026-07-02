d2bf11d636d9c5241354776f003af80883b254fe
rawaa999 <chaouchrawaa5@gmail.com>
2026-07-02 01:42:48 +0100

    Railway MySQL connection

--- src/main/java/util/DBConnection.java
+++ src/main/java/util/DBConnection.java
@@ -9,10 +9,21 @@ public class DBConnection {
     public static Connection getConnection() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            String host = System.getenv("MYSQLHOST") != null ? 
+                System.getenv("MYSQLHOST") : "localhost";
+            String port = System.getenv("MYSQLPORT") != null ? 
+                System.getenv("MYSQLPORT") : "3306";
+            String db = System.getenv("MYSQLDATABASE") != null ? 
+                System.getenv("MYSQLDATABASE") : "smartpharma";
+            String user = System.getenv("MYSQLUSER") != null ? 
+                System.getenv("MYSQLUSER") : "root";
+            String pass = System.getenv("MYSQLPASSWORD") != null ? 
+                System.getenv("MYSQLPASSWORD") : "";
+
             cn = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/smartpharma",
-                "root",
-                ""
+                "jdbc:mysql://" + host + ":" + port + "/" + db,
+                user, pass
             );
             System.out.println("Connexion MySQL reussie !");
         } catch (Exception e) {
